cd38077b97aae9864c2eb2329c2a5617e063e93f
RhodriJ9 <rhodri23@hotmail.co.uk>
2024-05-26 22:47:33 +0100

    refactor fields data

--- src/components/Account/RegisterForm.vue
+++ src/components/Account/RegisterForm.vue
@@ -25,25 +25,34 @@ export default defineComponent({
   },
 
   computed: {
+    validation() {
+      return {
+        name: Yup.string().required(this.t('validation.required', { field: this.t('fields.name') })).min(2, this.t('validation.min', { field: this.t('fields.name'), min: 2 })),  
+        email: Yup.string().email(this.t('validation.email')).required(this.t('validation.required', { field: this.t('fields.email') })),
+        password: Yup.string().min(8, this.t('validation.password.min', { min: 8 })).matches(/\d/, this.t('validation.password.matches')).required(this.t('validation.required', { field: this.t('fields.password') })),
+        choiceOfService: Yup.string().required(this.t('validation.required', { field: this.t('fields.choiceOfService') })),
+        specifyOtherService: Yup.string().required(this.t('validation.required', { field: this.t('fields.specifyOtherService') })),
+        dateOfBirth: Yup.date().transform((value, originalValue) => (originalValue === '' ? null : value)).nullable().max(new Date(), this.t('validation.maxDate', { field: this.t('fields.dateOfBirth') })),
+        termsAndConditions: Yup.string().required(this.t('validation.required', { field: this.t('fields.termsAndConditions') }))
+      };
+    },
+
     fields() {
       // This can be received via API
       return [
-        new Field(0, this.t('fields.name'), 'text', true, 'w-full', Yup.string().required(this.t('validation.required', { field: this.t('fields.name') })).min(2, this.t('validation.min', { field: this.t('fields.name'), min: 2 }))),
-        new Field(0, this.t('fields.email'), 'text', true, 'w-full', Yup.string().email(this.t('validation.email')).required(this.t('validation.required', { field: this.t('fields.email') }))),
-        new Field(0, this.t('fields.password'), 'password', true, 'w-full', Yup.string().min(8, this.t('validation.password.min', { min: 8 })).matches(/\d/, this.t('validation.password.matches')).required(this.t('validation.required', { field: this.t('fields.password') }))),
+        new Field(0, this.t('fields.name'), 'text', true, 'w-full', this.validation.name),
+        new Field(0, this.t('fields.email'), 'text', true, 'w-full', this.validation.email),
+        new Field(0, this.t('fields.password'), 'password', true, 'w-full', this.validation.password),
         
-        new Field(1, this.t('fields.choiceOfService'), 'select', true, 'w-full', Yup.string().required(this.t('validation.required', { field: this.t('fields.choiceOfService') })), [
+        new Field(1, this.t('fields.choiceOfService'), 'select', true, 'w-full', this.validation.choiceOfService, [
           new SelectOption(0, this.t('fields.webDevelopment')), 
           new SelectOption(1, this.t('fields.mobileDevelopment')),
           new SelectOption(2, this.t('fields.seoServices')),
           new SelectOption(3, this.t('fields.other')),
         ]),
-        new Field(1, this.t('fields.specifyOtherService'), 'text', true, 'w-full', Yup.string().required(this.t('validation.required', { field: this.t('fields.specifyOtherService') })), null, new FieldDependancy(0,3,'3')),
-        new Field(1, this.t('fields.dateOfBirth'), 'date', true, 'w-full', Yup.date()
-          .transform((value, originalValue) => (originalValue === '' ? null : value))
-          .nullable()
-          .max(new Date(), this.t('validation.maxDate', { field: this.t('fields.dateOfBirth') }))),
-        new Field(1, this.t('fields.termsAndConditions'), 'checkbox', true, 'w-full', Yup.string().required(this.t('validation.required', { field: this.t('fields.termsAndConditions') }))),
+        new Field(1, this.t('fields.specifyOtherService'), 'text', true, 'w-full', this.validation.specifyOtherService, null, new FieldDependancy(0,3,'3')),
+        new Field(1, this.t('fields.dateOfBirth'), 'date', true, 'w-full', this.validation.dateOfBirth),
+        new Field(1, this.t('fields.termsAndConditions'), 'checkbox', true, 'w-full', this.validation.termsAndConditions),
       ];
     },
   },
