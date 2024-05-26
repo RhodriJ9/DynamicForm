9e222f9808ac11141771a68d0ecf29720186ca7c
RhodriJ9 <rhodri23@hotmail.co.uk>
2024-05-26 18:07:46 +0100

    Primary button component

---
+++ src/components/Buttons/PrimaryButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button
+    :type="type"
+    class="font-heading bg-blue-600 hover:bg-blue-500 focus:ring-2 focus:ring-blue-300 focus:outline-none rounded-full px-5 py-4 text-white text-sm leading-none cursor-pointer transition-all duration-300"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  props: {
+    text: {
+      type: String,
+      default: ''
+    },  
+    type: {
+      type: String,
+      default: ''
+    },
+  },
+})
+</script>
