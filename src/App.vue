77ebc876d7088f1196b7f509cde851b42acbb3ec
RhodriJ9 <rhodri23@hotmail.co.uk>
2024-05-26 18:33:42 +0100

    Register form with inital(stage 0) fields

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
-<script setup lang="ts"></script>
-
 <template>
   <div class="flex h-screen w-full">
-    <div class="w-6/12 h-full">test</div>
-    <div class="w-6/12 h-full bg-gradient-to-br from-blue-400 to-blue-500">
-      dsds
+    <div class="w-6/12 h-full flex items-center justify-center">
+      <RegisterForm />
     </div>
+    <div class="w-6/12 h-full bg-gradient-to-br from-blue-400 to-blue-500"></div>
   </div>
 </template>
+
+<script>
+import { defineComponent } from 'vue'
+import RegisterForm from './components/Account/RegisterForm.vue';
+export default defineComponent({
+  components: {
+    RegisterForm
+  },
+})
+</script>
---
+++ src/components/Account/RegisterForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    Register
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import Field from '@/classes/Field.js'
+import * as Yup from 'yup'
+
+export default defineComponent({
+  computed: {
+    fields() {
+      // This can be received via API
+      return [
+        new Field(0, 'Name', 'text', true, 'w-full', Yup.string().required('Name is required').min(2, 'Name must be at least 2 characters')),
+        new Field(0, 'Email', 'text', true, 'w-full', Yup.string().email('Valid email is required').required('Email is required')),
+        new Field(0, 'Password', 'password', true, 'w-full', Yup.string().min(8, 'Password must be at least 8 characters').matches(/\d/, 'Password must contain at least one number').required('Password is required')),
+      ];
+    },
+  }
+})
+</script>
