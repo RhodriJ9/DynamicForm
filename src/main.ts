930c3d5d7335444c3659c860ecbb99d99776d204
RhodriJ9 <rhodri23@hotmail.co.uk>
2024-05-26 18:09:14 +0100

    Initialize i18n

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import i18n from './utils/i18n'
 
-createApp(App).mount('#app')
+createApp(App).use(i18n).mount('#app')
